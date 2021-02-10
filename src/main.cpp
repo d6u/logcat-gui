9646f192b68d91506ce19e28577d989b8bc14b1e
Daiwei Lu (AR/VR) <daiwei@fb.com>
2021-02-10 00:22:01 -0800

    use folly subprocess fd

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,35 @@
 // #include <vector>
 
 using namespace std;
+using namespace folly;
 
 int main(int argc, char *argv[]) {
-  folly::Subprocess("adb logcat -b all -T 10 -d").wait();
-  cout << "Hello World!" << endl;
+  Subprocess proc(
+    vector<string>{"ls", "-al"}, 
+    Subprocess::Options().pipeStdout().usePath());
+
+  // We couldn't use communicate here because it blocks until the
+  // stdout/stderr is closed.
+  char *line_buf = nullptr;
+  size_t line_buf_size = 0;
+  ssize_t line_size;
+
+  FILE *fd = fdopen(proc.stdoutFd(), "r");
+  
+  line_size = getline(&line_buf, &line_buf_size, fd);
+  
+  while (line_size >= 0) {
+    printf("line: chars=%06zd, buf size=%06zu, contents: %s",
+        line_size, line_buf_size, line_buf);
+    line_size = getline(&line_buf, &line_buf_size, fd);
+  }
+
+  delete line_buf;
+  fclose(fd);
+
+  // const auto [stdout, stderr] = proc.communicate();
+
+  proc.wait();
+
   return 0;
 }
