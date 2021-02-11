625bb369701384ff0c8d21e3e5bc78c273447118
Daiwei Lu <daiwei@fb.com>
2021-02-10 20:33:31 -0800

    terminate adb process

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <folly/Subprocess.h>
+#include <folly/io/async/AsyncSignalHandler.h>
+#include <folly/io/async/EventBase.h>
 #include <iostream>
-#include <signal.h>
 // #include <ncurses.h>
 // #include <algorithm>
 // #include <istream>
@@ -15,43 +16,54 @@
 using namespace folly;
 
 void outputLines(int fd) {
+  FILE *file_handle = fdopen(fd, "r");
+
   char *line_buf = nullptr;
   size_t line_buf_size = 0;
-  ssize_t line_size;
-
-  FILE *file = fdopen(fd, "r");
-
-  line_size = getline(&line_buf, &line_buf_size, file);
+  ssize_t line_size = getline(&line_buf, &line_buf_size, file_handle);
 
   while (line_size >= 0) {
     printf("line: chars=%06zd, buf size=%06zu, contents: %s", line_size,
            line_buf_size, line_buf);
-    line_size = getline(&line_buf, &line_buf_size, file);
+    line_size = getline(&line_buf, &line_buf_size, file_handle);
   }
 
   delete line_buf;
-  fclose(file);
+  fclose(file_handle);
 }
 
-std::shared_ptr<Subprocess> proc;
+class SignalHandler : public AsyncSignalHandler {
+public:
+  SignalHandler(EventBase *eb, std::shared_ptr<Subprocess> proc)
+      : AsyncSignalHandler(eb), proc_(proc) {}
 
-void signal_callback_handler(int signum) {
-  std::cout << "Receive signal: " << signum << std::endl;
-  if (signum == SIGINT && proc != nullptr) {
-    proc->terminate();
+  void signalReceived(int /* signum */) noexcept override {
+    std::cout << std::endl << "Stopping process..." << std::endl;
+    getEventBase()->terminateLoopSoon();
+    proc_->terminate();
   }
-}
+
+private:
+  std::shared_ptr<Subprocess> proc_;
+};
 
 int main(int argc, char *argv[]) {
-  signal(SIGINT, signal_callback_handler);
-
-  proc = std::make_shared<Subprocess>(
-      std::vector<std::string>{"adb", "-s", "emulator-5554", "logcat"},
+  auto adb_proc = std::make_shared<Subprocess>(
+      std::vector<std::string>{"adb", "-s", "emulator-5554", "logcat", "-b",
+                               "all", "-T", "10"},
       Subprocess::Options().pipeStdout().usePath());
 
-  outputLines(proc->stdoutFd());
+  auto thread = std::thread([adb_proc]() {
+    EventBase eventBase;
+    SignalHandler handler(&eventBase, adb_proc);
+    handler.registerSignalHandler(SIGINT);
+    eventBase.loopForever();
+  });
 
-  proc->wait();
+  outputLines(adb_proc->stdoutFd());
+
+  adb_proc->wait();
+  thread.join();
 
   return 0;
 }
